5a84eb3e5d96f132429a7303116d235140782d7d
Anudeep Muppalla <anudeepmuppalla@gmail.com>
2025-11-21 20:03:32 +0530

    Initialize FastAPI application with CORS middleware and authentication router

---
+++ server/app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.auth import router as auth_router
+import os
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+
+app = FastAPI(title="InboxAI Server", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth_router)
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to the InboxAI Server!"}
