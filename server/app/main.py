b1a62b74845826f0f5a0e4c5a67c34cba29d00da
Anudeep Muppalla <anudeepmuppalla@gmail.com>
2025-11-21 21:03:33 +0530

    Include Gmail test router in the main application

--- server/app/main.py
+++ server/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.auth import router as auth_router
+from app.api.gmail_test import router as gmail_test_router
 import os
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
@@ -16,6 +17,7 @@ app.add_middleware(
 )
 
 app.include_router(auth_router)
+app.include_router(gmail_test_router)
 
 @app.get("/")
 def root():
